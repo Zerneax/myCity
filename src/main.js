a5c8ae9092c67a49ffcd8e7442a9b418764860a1
Zerneax <d.chris59@gmail.com>
2018-09-18 21:37:53 +0200

    add semantic ui to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import SuiVue from 'semantic-ui-vue'
 
+import 'semantic-ui-css/semantic.min.css'
+
+Vue.use(SuiVue);
 Vue.config.productionTip = false
 
 new Vue({
