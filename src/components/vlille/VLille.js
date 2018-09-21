1fe0eb48b724285caa5fff2c625692692ca0f31f
Zerneax <d.chris59@gmail.com>
2018-09-21 22:59:20 +0200

    call to opendata vlille

--- src/components/vlille/VLille.js
+++ src/components/vlille/VLille.js
@@ -1,7 +1,47 @@
+import axios from 'axios'
+
 export default {
   name: 'vLille',
   data() {
     return {
+      vlille: [],
+      nbhits : 0,
+      start: 0
     };
+  },
+  created() {
+    let url = "https://opendata.lillemetropole.fr/api/records/1.0/search//?dataset=vlille-realtime";
+    axios(url)
+    .then(response => {
+      this.nbhits = response.data.nhits;
+      this.vlille = response.data.records;
+    })
+    .catch(e => {
+      console.log("Erreur !!");
+    });
+  },
+  methods: {
+    suivant() {
+      this.start += 10;
+      let url = "https://opendata.lillemetropole.fr/api/records/1.0/search//?dataset=vlille-realtime&start=" + this.start;
+      axios(url)
+      .then(response => {
+        this.vlille = response.data.records;
+      })
+      .catch(e => {
+        console.log("Erreur !!");
+      });
+    },
+    precedent() {
+      this.start -=10;
+      let url = "https://opendata.lillemetropole.fr/api/records/1.0/search//?dataset=vlille-realtime&start=" + this.start;
+      axios(url)
+      .then(response => {
+        this.vlille = response.data.records;
+      })
+      .catch(e => {
+        console.log("Erreur !!");
+      });
+    }
   }
 };
--- src/components/vlille/VLille.vue
+++ src/components/vlille/VLille.vue
@@ -1,6 +1,24 @@
 <template>
   <body>
-    <p> First Page </p>
+    <div class="ui two column grid">
+      <div class="column">
+        <div class="ui">
+          <sui-list v-for="station in vlille" :key="station.recordid" >
+            <sui-list-item>
+              <p> Commune : {{station.fields.commune}} </p>
+              <p> Nom de la station : {{station.fields.nom}} </p>
+              <p> Nombre de velos disponibles : {{station.fields.nbvelosdispo}} </p>
+              <p> Nombre de places disponibles : {{station.fields.nbplacesdispo}} </p>
+            </sui-list-item>
+            <div class="ui divider"></div>
+          </sui-list>
+          <button class="ui button" v-on:click="precedent()" :disabled="start == 0"> Précédent </button>
+          <button class="ui button" v-on:click="suivant()" :disabled="(nbhits - (start + 10)) <= 0"> Suivant </button>
+        </div>
+      </div>
+      <div class="column">
+      </div>
+    </div>
   </body>
 </template>
 
